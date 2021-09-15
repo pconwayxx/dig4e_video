6d75decfe1af5cd600fbcd952c55744c2f4fd03f
Jordan Gorzalski <gorjor@umich.edu>
2021-09-15 14:34:04 -0400

    Changes to Index

--- index.php
+++ index.php
@@ -13,61 +13,36 @@ require "nav.php";
 require_once "tsugi/admin/sanity-db.php";
 ?>
 <div id="container">
+<!--
 <div style="margin-left: 10px; float:right">
 <iframe width="400" height="225" src="" frameborder="0" allowfullscreen></iframe>
 </div>
-<h1>Digitization for Everybody - Imaging</h1>
+-->
+<div id="grid-container" style="display: grid; grid-template-columns: 10% 90%;">
+<img src="https://image.dig4e.com/images/imaginglogo.png" alt="Dig4E Imaging Logo" style = "width: 8em; height: 8em; grid-column: 1/2; border: 10 px; margin: 20px;"/>
+<h1 style="color: #575294; font-weight: 600; font-family: Raleway, Sans-Serif; font-size: 3em;grid-column: 2/2; padding-top: 1em; padding-left: 1em;">Digitization for Everybody - Video</h1>
+</div>
+<div id="container" style="dispalay: container; background-color: #575294; height: 1em; margin-top: 1em; margin-bottom: 1em;">
+</div>
+<div id="container" style= "color: #555555; font-family: Lusitana, Serif; font-weight: 500; font-size: 18px; margin-bottom: 1em;" >
+In the Dig4E Video module, you learn about the present state of standards and recommended best practices for transforming the complex analog signals stored on videotape into encoded digital files wrapped in a standardized digital container. Some of the standards for digitization are stable while others are emergent, incompatible, or inconsistent. The module will help you understand where the technical uncertainties lie. Cultural heritage organizations should proceed with digitization while exercising caution.</div>
+<div id="container" style="dispalay: container; background-color: #575294; height: 1em; margin-top: 1em; margin-bottom: 1em;">
+</div>
+<div id="grid-container" style="display: grid; grid-template-columns: 30% 70%;">
+<iframe width="300" height="200" src="https://www.youtube.com/embed/XHOmBV4js_E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p style="color: #555555; font-family: Lusitana, Serif; font-weight: 500; font-size: 18px;grid-column: 2/2; padding-top: 1em;">This video introduces the Video module in the broader context of the risks and challenges to audiovisual heritage. The primary points are to balance media complexity and the absence of full consensus on standards, on the one hand, with the risk of inaction and the critical value of emergent digital processes. The video introduction emphasizes documentation and transparency and summarizes the top level learning goals for the Video module.</p>
+</div>
+<div id="container" style="dispalay: container; background-color: #575294; height: 1em; margin-top: 1em; margin-bottom: 1em;">
+</div>
+<div id="grid-container" style ="display: grid; grid-template-columns: 50% 50%; grid-row-gap: 1em;" hover="background-color: rgba(158, 79, 0, 0.2);">
+<a href="https://image.dig4e.com/lessons"><img src="https://image.dig4e.com/images/lessons.png" alt="Lessons Logo" style= "width: 10em; height 10em; position: inline; margin-left: 35%; margin-right: 35%; hover: #9E4F002E"/></a>
+<a href="https://image.dig4e.com/assignments"><img src="https://image.dig4e.com/images/quizzes.png" alt="Quizzes Logo" style= "width: 10em; height 10em; position: inline; margin-left: 35%; margin-right: 50%;"/></a>
+<a href="https://image.dig4e.com/lessons" style="color: #00274C; font-family: Raleway, Sans-Serif; font-weight: 500; font-size: 2em; position: inline; margin-left: 39%; margin-right: 46%; grid-column: 1; grid-row: 2">Lessons</a>
+<a href="https://image.dig4e.com/assignments" style="color: #00274C; font-family: Raleway, Sans-Serif; font-weight: 500; font-size: 2em; position: inline; margin-left: 39%; margin-right: 46%; grid-column: 2; grid-row: 2">Quizzes</a>
+</div>
 <?php if ( isset($_SESSION['id']) ) { ?>
-<p>
-Welcome to our Massive Open Online Course (MOOC). Now that you have logged in, you have access to
-course-style features of this web site.
-
 <?php } else { ?>
-<p>
-Hello and welcome to my site where you can learn about the present state of standards and recommended
-best practices for creating digital still images from photographic and graphic source materials.
-<br>
-You can use this web site many different ways:
-<ul>
-<li>
-You browse my videos and course materials under <a href="lessons">Lessons</a>.  The materials
-I have developed
-for this class are all provided with a Creative Commons license so you can download or link to
-them to incorporate them into your own teaching if you like.</li>
-We take your privacy seriously on this site, you can review our
-<a href="privacy">Privacy Policy</a> for more details.
-</li>
-</ul>
 <?php } ?>
-This site uses <a href="http://www.tsugi.org" target="_blank">Tsugi</a>
-framework to embed a learning
-management system into this site and handle the autograders.
-If you are interested in collaborating
-to build these kinds of sites for yourself, please see the
-<a href="http://www.tsugi.org" target="_blank">tsugi.org</a> website.
-</p>
-<h3>Textbook</h3>
-<p>
-I am using an open textbook
-<a href="https://learn.shayhowe.com/html-css/"
-target="_new">
-Learn to Code HTML & CSS
-</a>
-by Shay Howe.
-All of the electronic copies
-of the book are free. Print copies of the book are available on
-<a href="https://www.informit.com/store/learn-to-code-html-and-css-develop-and-style-websites-9780321940520?ranMID=24808"
-target="_blank">Pearson</a>.
-</p>
-<h3>Copyright</h3>
-<p>
-All this material produced by Colleen Van Lent (including audio and video)
-is Copyright Creative Commons Attribution 3.0
-unless otherwise indicated.
-</p>
-<p>
---  Colleen
-</p>
 <!--
 <?php
 echo("IP Address: ".Net::getIP()."\n");
